6e008124de2c8656b74177a66b6d38aca64e4772
Jonathan Ströbele <mail@jonathanstroebele.de>
2016-02-20 00:28:49 +0100

    feat: check for pagination to fetch all stars

--- app.php
+++ app.php
@@ -10,13 +10,8 @@ $cache_treshold   = 3600 * 24; // in seconds
 // check if we hafe a cache
 // if not load stars from github API
 if (file_exists('cache.json') && filemtime('cache.json') > (time() - $cache_treshold)) {
-
-	$resp = file_get_contents('cache.json');
-
+	$json = json_decode(file_get_contents('cache.json'), true);
 } else {
-
-
-
 	// get starred URL
 	$id          = file_get_contents('userid.txt');
 	$user_name   = explode("\n",$id,2);
@@ -35,10 +30,33 @@ if (file_exists('cache.json') && filemtime('cache.json') > (time() - $cache_tres
 	$resp        = substr($resp, $header_size);
 	$http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
+
+	$json = json_decode($resp, true);
+
+	// check if there are headers indication pagination
+	// => make multiple requests to fetch ALL the stars.
+	if (preg_match('/Link:.*([0-9]+)>; rel="last"/', $header, $m)) {
+		$last_page = (int) $m[1];
+
+		for ($i = 2; $i <= $last_page; $i++) {
+			$page_url = $starred_url . '?page=' . $i;
+			curl_setopt($curl, CURLOPT_URL, $page_url);
+			$resp     = curl_exec($curl);
+
+			$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+			$header      = substr($resp, 0, $header_size);
+			$resp        = substr($resp, $header_size);
+			$http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+			$json_loop   = json_decode($resp, true);
+			$json        = array_merge($json, $json_loop);
+		}
+	}
+	
 	curl_close($curl);
 
 	// cache response
-	file_put_contents('cache.json', $resp);
+	file_put_contents('cache.json', json_encode($json, JSON_PRETTY_PRINT));
 
 
 	// determine rate limit reset
@@ -46,12 +64,11 @@ if (file_exists('cache.json') && filemtime('cache.json') > (time() - $cache_tres
 	if (preg_match('/X-RateLimit-Remaining: ([0-9]+)/', $header, $m)) {
 		$api_remain_limit = $m[1];
 	}
-
 }
 
 
 
-$data = json_decode($resp, true);
+$data = $json;
 $xml = "<?xml version=\"1.0\"?>\n<items>\n";
 
 //
